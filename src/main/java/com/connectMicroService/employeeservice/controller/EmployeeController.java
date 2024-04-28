339d21edb813f276abc51e474bfe0a9d3946f82a
kapilan1 <kapilan@codelantic.com>
2024-04-28 10:49:59 +0530

    initial commit

---
+++ src/main/java/com/connectMicroService/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.connectMicroService.employeeservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@Slf4j
+public class EmployeeController {
+
+    @GetMapping("/employee-detail")
+    public String displayEmployeeDetail(){
+        return "Name : Edwin , email :edwinmose@gmail.com";
+    }
+}
