0c6863877cce56e9a7d44499d5b283c7c75b41a4
kapilan1 <kapilan@codelantic.com>
2024-04-28 16:44:59 +0530

    dependency added + maven plugin added

--- src/main/java/com/connectMicroService/employeeservice/Controller/advice/ControllerExceptionHandler.java
+++ src/main/java/com/connectMicroService/employeeservice/Controller/advice/ControllerExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.connectMicroService.employeeservice.Controller.advice;
 
-import com.connectMicroService.employeeservice.Dto.ResponseMessageDto;
 import com.connectMicroService.employeeservice.Exception.ServiceException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
@@ -11,7 +10,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -19,10 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
